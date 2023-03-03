b938e1f5d6b9c3a0a8d319e748b476e22579af16
Luis Vazquez <luisito986.22@gmail.com>
2023-03-02 19:36:24 -0600

    correcion del nombre de variables de entorno

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,5 @@
 // const {Sequelize} = require('sequelize');
-import {Sequelize} from "sequelize";
+import Sequelize from "sequelize";
 import config from "../config";
 
 const db = new Sequelize(config.db.name, config.db.user, config.db.password, {
